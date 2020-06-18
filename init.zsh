0ca032ed2bc78c6f506928b375db9318d61b6ca0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-18 00:40:54 -0400

    fix(brew): install what I want, it stablized (#6)

--- init.zsh
+++ init.zsh
@@ -15,9 +15,9 @@ p6df::modules::docker::version() { echo "0.0.1" }
 ######################################################################
 p6df::modules::docker::deps() {
     ModuleDeps=(
-	akarzim/zsh-docker-aliases
-	robbyrussell/oh-my-zsh:plugins/docker
-	robbyrussell/oh-my-zsh:plugins/docker-compose
+   	  akarzim/zsh-docker-aliases
+	    robbyrussell/oh-my-zsh:plugins/docker
+	    robbyrussell/oh-my-zsh:plugins/docker-compose
     )
 }
 
@@ -30,14 +30,18 @@ p6df::modules::docker::deps() {
 ######################################################################
 p6df::modules::docker::external::brew() {
 
-    local formulae
-    for formulae in $(brew search docker |egrep -v 'Formulae|Casks'  |sort -u); do
-	brew install $formulae
-    done
-
-    for formulae in $(brew search docker |egrep -v 'Formulae|Casks'  |sort -u); do
-	brew cask install $formulae
-    done
+  brew cask install docker
+  brew cask install docker-toolbox
+  brew install docker-swarm
+  brew install docker-compose
+  brew install docker-compose-completion
+  brew install docker-credential-helper
+  brew install docker-credential-helper-ecr
+  brew install docker-gen
+  brew install docker-ls
+  brew install docker-slim
+  brew install docker-squash
+  brew install  dockerize
 }
 
 ######################################################################
